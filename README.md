2a1369cd91d7ca01c884d8e6af1248342675f83c
David Arenas <davejs136@gmail.com>
2023-08-24 00:24:58 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# serapi-bridge
+
+The goal of this API is to provide a simple bridge between `serapi` and front-end applications
+
+### Getting started
+
+1. Clone the repository `https://github.com/Dave136/serapi-bridge`
+2. Create a `.env` file typing `cp .env.example .env`
+3. Run the project with `deno tasks dev` 
+
+### Author
+
+[David Arenas](https://github.com/Dave136)
---
+++ mod.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono';
+import { cors } from 'hono:cors';
+import { getJson } from 'serpapi';
+import 'dotenv';
+
+const PORT = Deno.env.get('PORT');
+const API_KEY = Deno.env.get('API_KEY');
+
+const app = new Hono();
+
+app.use('*', cors());
+
+app.get('/', (c) => c.json({ message: 'SerpAPI bridge - Dave136' }));
+
+app.get('/search', async (c) => {
+  const { q } = c.req.query();
+  const response = await getJson({
+    q,
+    engine: 'google_image',
+    api_key: API_KEY,
+  });
+
+  return c.json(response);
+});
+
+Deno.serve({ port: Number(PORT) || 3030 }, app.fetch);
